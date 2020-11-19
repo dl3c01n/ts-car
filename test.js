316d863959aa55a6688977b9f4586baaa4ce6284
dl3c0in <david.lecoin@yahoo.com>
2020-11-19 15:21:59 +0100

    inheritance cars

--- test.js
+++ test.js
@@ -1,24 +1,69 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Car = /** @class */ (function () {
-    function Car(plate, seaters, brand, colour) {
+    function Car(plate, seaters, brand, colour, carburant) {
         var _this = this;
         this.generateCar = function () {
             console.log("La marque de ma voiture est : " + _this.brand);
             console.log("Il y a  " + _this.seaters + " de passagers");
             console.log("La plaque de ma voiture est : " + _this.plate);
             console.log("Ma voiture est : " + _this.colour);
+            console.log("Ma voiture est : " + _this.carburant);
         };
         this.plate = plate;
         this.seaters = seaters;
         this.brand = brand;
         this.colour = colour;
+        this.carburant = carburant;
     }
     return Car;
 }());
-var car1 = new Car("AA-000-BB", 3, "Mercedes", "Noir Mat");
-var car2 = new Car("JJ-666-DD", 3, "Renault", "Jaune");
-var car3 = new Car("CC-999-ZZ", 3, "Aston Martin", "Gris");
-car1.generateCar();
-car2.generateCar();
-car3.generateCar();
+var electricCar = /** @class */ (function (_super) {
+    __extends(electricCar, _super);
+    function electricCar(plate, seaters, brand, colour, carburant) {
+        return _super.call(this, plate, seaters, brand, colour, carburant) || this;
+    }
+    return electricCar;
+}(Car));
+var fuelCar = /** @class */ (function (_super) {
+    __extends(fuelCar, _super);
+    function fuelCar(plate, seaters, brand, colour, carburant) {
+        return _super.call(this, plate, seaters, brand, colour, carburant) || this;
+    }
+    return fuelCar;
+}(Car));
+var dieselCar = /** @class */ (function (_super) {
+    __extends(dieselCar, _super);
+    function dieselCar(plate, seaters, brand, colour, carburant) {
+        return _super.call(this, plate, seaters, brand, colour, carburant) || this;
+    }
+    return dieselCar;
+}(Car));
+var pedalCar = /** @class */ (function (_super) {
+    __extends(pedalCar, _super);
+    function pedalCar(plate, seaters, brand, colour, carburant) {
+        return _super.call(this, plate, seaters, brand, colour, carburant) || this;
+    }
+    return pedalCar;
+}(Car));
+var electric = new electricCar("AA-000-BB", 3, "Mercedes", "Noir Mat", "Electrique");
+var fuel = new fuelCar("FF-666-BB", 3, "Renault", "Orange", "Essence");
+var diesel = new dieselCar("ZZ-123-VV", 3, "Peugeot", "Marron", "Disel");
+var pedal = new pedalCar("JJ-258-HJ", 3, "BMW", "Gris", "Pedale");
+electric.generateCar();
+fuel.generateCar();
+diesel.generateCar();
+pedal.generateCar();
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,14 +1,16 @@
 class Car {
-    private plate : string;
-    private seaters: number;
-    private brand: string;
-    private colour: string;
+    public plate : string;
+    public seaters: number;
+    public brand: string;
+    public colour: string;
+    public carburant: string;
 
-    constructor(plate: string, seaters: number, brand: string, colour: string){
+    constructor(plate: string, seaters: number, brand: string, colour: string, carburant: string){
         this.plate = plate;
         this.seaters = seaters;
         this.brand = brand;
         this.colour = colour;
+        this.carburant = carburant;
     }
 
     public generateCar = () :void => {
@@ -16,13 +18,40 @@ class Car {
         console.log(`Il y a  ${this.seaters} de passagers`)
         console.log(`La plaque de ma voiture est : ${this.plate}`)
         console.log(`Ma voiture est : ${this.colour}`)
+        console.log(`Ma voiture est : ${this.carburant}`)
     } 
 }
 
-let car1 = new Car("AA-000-BB", 3, "Mercedes", "Noir Mat") 
-let car2 = new Car("JJ-666-DD", 3, "Renault", "Jaune") 
-let car3 = new Car("CC-999-ZZ", 3, "Aston Martin", "Gris") 
+class electricCar extends Car {
+    constructor(plate: string, seaters: number, brand: string, colour: string, carburant: string){
+        super(plate, seaters, brand, colour, carburant);
+    }
+}
 
-car1.generateCar()
-car2.generateCar()
-car3.generateCar()
+class fuelCar extends Car {
+    constructor(plate: string, seaters: number, brand: string, colour: string, carburant: string){
+        super(plate, seaters, brand, colour, carburant);
+    }
+}
+
+class dieselCar extends Car {
+    constructor(plate: string, seaters: number, brand: string, colour: string, carburant: string){
+        super(plate, seaters, brand, colour, carburant);
+    }
+}
+
+class pedalCar extends Car {
+    constructor(plate: string, seaters: number, brand: string, colour: string, carburant: string){
+        super(plate, seaters, brand, colour, carburant);
+    }
+}
+
+let electric = new electricCar("AA-000-BB", 3, "Mercedes", "Noir Mat", "Electrique") 
+let fuel = new fuelCar("FF-666-BB", 3, "Renault", "Orange", "Essence") 
+let diesel = new dieselCar("ZZ-123-VV", 3, "Peugeot", "Marron", "Disel") 
+let pedal = new pedalCar("JJ-258-HJ", 3, "BMW", "Gris", "Pedale") 
+
+electric.generateCar()
+fuel.generateCar()
+diesel.generateCar()
+pedal.generateCar()
